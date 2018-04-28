8ea70a5f9e8d92b5bad50db2233ca8cdb855227f
luojiahai <ffreylaw@gmail.com>
2018-04-28 19:54:37 +1000

    db.py gives example to get document

--- db.py
+++ db.py
@@ -1,10 +1,12 @@
 import couchdb
 import json
 
-couch = couchdb.Server()
+couch = couchdb.Server('http://115.146.84.252:5432/')
 db = couch['tweets']
 
 # get all the docs in db 'tweets'
 for id in db:
-    doc = db[id]
-    print(doc)
+    doc = db[id] # to get the document by id
+    text = doc['text'] # to get the text field in one tweet document
+    print(text)
+    break
