e688837eb7cceedf6b8b7e8942a700c705b9a0e2
luojiahai <ffreylaw@gmail.com>
2018-05-01 22:08:54 +1000

    update

--- twitter/filter.py
+++ twitter/filter.py
@@ -5,9 +5,14 @@ db = couch['tweets']
 
 for id in db:
     doc = db[id] # to get the document by id
-    if (not doc['coordinates']):
+    try:
+        if (not doc['coordinates']):
+            db.delete(doc)
+        elif ((doc['coordinates']['coordinates'][0] >= 113.6594 and doc['coordinates']['coordinates'][0] <= 153.61194) and (doc['coordinates']['coordinates'][1] >= -43.00311 and doc['coordinates']['coordinates'][1] <= -12.46113)):
+            None
+        else:
+            db.delete(doc)
+    except KeyError as e:
         db.delete(doc)
-    elif ((doc['coordinates']['coordinates'][0] >= 113.6594 and doc['coordinates']['coordinates'][0] <= 153.61194) and (doc['coordinates']['coordinates'][1] >= -43.00311 and doc['coordinates']['coordinates'][1] <= -12.46113)):
-        None
-    else:
-        db.delete(doc)
+        continue
+
