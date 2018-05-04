2b32750e37eb072fb7c0f9d7f0f75196262f3b50
luojiahai <ffreylaw@gmail.com>
2018-05-04 14:57:37 +1000

    batch convert

---
+++ twitter/batch_convert.py
@@ -0,0 +1,26 @@
+import couchdb
+import requests
+import json
+
+couch = couchdb.Server('http://115.146.95.53:5432/')
+db = couch['tweets']
+
+skip = 0
+limit = 10
+
+while True:
+    uri = "http://readonly:ween7ighai9gahR6@45.113.232.90/couchdbro/twitter/_design/twitter/_view/summary?include_docs=true&reduce=false&skip={}&limit={}".format(str(skip), str(limit))
+    response = requests.get(uri)
+
+    data = response.json()
+    rows = data['rows']
+
+    for row in rows:
+        try:
+            doc = row['doc']
+            db[doc['_id']] = doc
+        except Exception as e:
+            print(e)
+            continue
+    
+    skip += limit
